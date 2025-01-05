a41c56fc89637ad3b8d75f7ef4c0933caa3ef8f5
Sardar Mudassar Ali Khan <55798521+SardarMudassarAliKhan@users.noreply.github.com>
2025-01-06 00:20:28 +0500

    Add EF Core to project and configure ApplicationDbContext
    Added Microsoft.EntityFrameworkCore package to the project.
    Updated ApplicationDbContext to use EF Core, including:
    - Imported necessary namespaces.
    - Defined ApplicationDbContext class inheriting from DbContext.
    - Added DbSet<User> property for Users.
    - Configured one-to-one relationship between User and UserProfile.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PerformanceAndMigrationsMVC.Model;
+
+namespace PerformanceAndMigrationsMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Profile)
+                .WithOne(p => p.User)
+                .HasForeignKey<UserProfile>(p => p.UserId);
+        }
+    }
+}
